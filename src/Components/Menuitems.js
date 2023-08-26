ac46030a5bf9e677eb654e6109bfc6e68c3ad679
Sukalyan <sadhikary0055@gmail.com>
2023-08-26 13:03:41 +0530

    commited

---
+++ src/Components/Menuitems.js
@@ -0,0 +1,34 @@
+export  const Menuitems=[
+    {
+        title: "Home",
+        url: "/",
+        cName:"Nav-links",
+        icon:"fa-solid fa-house-user"
+    },
+    {
+        title: "About",
+        url: "/About",
+        cName:"Nav-links",
+        icon:"fa-solid fa-circle-info"
+    },
+    {
+        title: "Service",
+        url: "/Service",
+        cName:"Nav-links",
+        icon:"fa-solid fa-briefcase"
+    },
+
+    {
+        title: "Contact",
+        url: "/Contact",
+        cName:"Nav-links",
+        icon:"fa-solid fa-address-book"
+    },
+
+    {
+        title: "Sign up",
+        url: "/signup",
+        cName:"Nav-links-mobile",
+       
+    },
+]
--- src/Components/Navber.js
+++ src/Components/Navber.js
@@ -1,19 +1,37 @@
 import React, { Component } from "react";
-import {Link} from 'react-router-dom';
+import { Link } from "react-router-dom";
 import "./Navber.css";
+import { Menuitems } from "./Menuitems";
 
 class Navber extends Component {
+//this for responsive navber toggle icon
+state={clicked:false};
+handleClicked=()=>{
+  this.setState({clicked : !this.state.clicked})
+}
+
   render() {
     return (
       <nav className="NavberItems">
         <h1 className="Navber-logo">Trippy</h1>
-        <ul className="nav-menu">
-          <li>
-            <Link to='\'>
-              <i class="fa-solid fa-house-user"></i>
-              Home
-            </Link>
-          </li>
+
+        <div className="menu-icon" onClick={this.handleClicked}>
+            <i className={this.state.clicked ? "fas fa-times" : "fas fa-bars"}></i>
+        </div>
+
+        <ul className={this.state.clicked ? 
+         "nav-menu active" : "nav-menu"}>
+          {Menuitems.map((item, index) => {
+            return (
+              <li key={index}>
+                <Link to="/" className={item.cName}>
+                  <i className={item.icon}></i>
+                  {item.title}
+                </Link>
+              </li>
+            );
+          })}
+          <button>Sign up</button>
         </ul>
       </nav>
     );
